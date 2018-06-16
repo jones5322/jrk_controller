eef0f542f5182e8890e7e47eba81ffc5901689c3
unknown <al5322@outlook.com>
2018-06-16 12:58:19 -0400

    modified the servo positions in publisher

--- scripts/jrk_test_pub.py
+++ scripts/jrk_test_pub.py
@@ -7,7 +7,7 @@ print("starting jrk_test_pub")
 def talker():
     jrk_pub = rospy.Publisher('jrk_target', std_msgs.msg.UInt16, queue_size=1)
     rospy.init_node('test_jrk_pub')
-    for i in [0, 800, 1200, 1500, 2000, 2500, 3000, 4000]:
+    for i in [0, 1200, 2500, 4000, 0]:
         print(i)
         jrk_pub.publish(i)
         rospy.sleep(10)
