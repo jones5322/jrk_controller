d38f4ca04e4ff897fecc67fdd1a9bffae15e10dd
Al Jones <al5322@outlook.com>
2018-06-18 01:29:39 +0000

    update joystick mapping to target - still in progress

--- scripts/jrk_joystick.py
+++ scripts/jrk_joystick.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
-# this program will subscribe to the joystick and output a target for the jrk motor controller to complete
 import rospy
 from sensor_msgs.msg import Joy
 
+def translate(sensor_val, in_from, in_to, out_from, out_to):
+    out_range = out_to - out_from
+    in_range = in_to - in_from
+    in_val = sensor_val - in_from
+    val=(float(in_val)/in_range)*out_range
+    out_val = out_from+val
+    return out_val
+
 def get_target (msg):
-    print msg.axes
+	joystick_output = msg.axes
+	target = translate(joystick_output[0], -1, 1, 4095, 0)
+	print 'left - right', joystick_output[0], 'target', int(target)
 
 rospy.init_node('my_jrk_joystick')
 sub = rospy.Subscriber ('/joy', Joy, get_target)
-r = rospy.Rate(1)  
-while not rospy.is_shutdown():  
-    r.sleep()
+r = rospy.Rate(1)
+while not rospy.is_shutdown():
+	r.sleep()
