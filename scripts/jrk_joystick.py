481cf3461aefd84eeab416d788c3f50c5c18ef41
Al Jones <al5322@outlook.com>
2018-06-19 00:58:51 +0000

    updated to subscribe to Twist for angular.z value

--- scripts/jrk_joystick.py
+++ scripts/jrk_joystick.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
-from sensor_msgs.msg import Joy
+#from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
 
 def translate(sensor_val, in_from, in_to, out_from, out_to):
     out_range = out_to - out_from
@@ -11,12 +12,16 @@ def translate(sensor_val, in_from, in_to, out_from, out_to):
     return out_val
 
 def get_target (msg):
-	joystick_output = msg.axes
-	target = translate(joystick_output[0], -1, 1, 4095, 0)
-	print 'left - right', joystick_output[0], 'target', int(target)
+    #joystick_output = msg.axes
+    #rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
+    #print msg.angular.z, type(msg.angular.z)
+    #joystick_output = msg.angular.z
+    target = translate(msg.angular.z, -1, 1, 4095, 0)
+    print 'left - right', msg.angular.z, 'target', int(target)
 
 rospy.init_node('my_jrk_joystick')
-sub = rospy.Subscriber ('/joy', Joy, get_target)
+#sub = rospy.Subscriber ('/joy', Joy, get_target)
+sub = rospy.Subscriber ('/cmd_vel', Twist, get_target)
 r = rospy.Rate(1)
 while not rospy.is_shutdown():
 	r.sleep()
